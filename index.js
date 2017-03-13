240f7b4658fffe855d1cbbe021012005f0b004e4
you21979 <you2197901@gmail.com>
2017-03-13 20:04:00 +0900

    mod

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+exports.Constant = require('./lib/constant');
 exports.RestClient = require('./lib/insight-rest-client');
 exports.StreamClient = require('./lib/insight-socketio-client');
 exports.batch = require('./lib/batch');
